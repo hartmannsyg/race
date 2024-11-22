f39db2121d192414c3571e1d0d2d03d0bcae4a62
hartmannsyg <153003450+hartmannsyg@users.noreply.github.com>
2024-11-22 22:23:11 +0800

    update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,39 +1,56 @@
-const url = atob("aHR0cHM6Ly9kYXRhLm5pbmpha2l3aS5jb20vYnRkNi9yYWNlcy9EZWNyeXB0ZWRfbTNoaHBwdWkvbGVhZGVyYm9hcmQ="); // Replace with your desired URL
 const fs = require('fs');
 // Function to fetch data from the URL
-async function fetchData(attempts = 0) {
+
+const MAX_ATTEMPTS = 3;
+
+async function fetchURL(url, attempts = 0) {
     try {
         const response = await fetch(url);
-        
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
-
         const res = await response.json(); // Assuming the response is JSON
-
         if (!res.success) throw new Error(`NK error: ${res.error}`);
 
-        // responseStorage.push(data); // Store the data
-        const scores = res.body.map((obj) => {
-            return obj.scoreParts[0].score;
-        });
-
-        let text = await fs.readFileSync('scores.json', 'utf-8');
-        let past = JSON.parse(text);
-        console.log(past);
-        past.push(scores);
-        
-        await fs.writeFileSync('scores.json', JSON.stringify(past));
-    
-        console.log("Data stored:", scores);
-
+        return res.body;
     } catch (error) {
-        if (attempts < 3) fetchData(attempts+1);
-        else console.error(`[${Date.now()}] Failed to fetch data in 3 tries:`, error);
-        
+        if (attempts < MAX_ATTEMPTS) fetchData(attempts+1);
+        else console.error(`[${Date.now()}] Failed to fetch ${url} in 3 tries:`, error);        
     }
-
-
 }
-fetchData(0)
-// Schedule the fetch every minute (60000 milliseconds)
-setInterval(fetchData, 60000);
+
+async function fetchLB(race) {
+    
+    const { start, leaderboard, end } = race
+
+    if (Date.now() < start) return;
+    if (Date.now() > end) return;
+
+    const lb = fetch(leaderboard)
+    const lb2 = fetch(leaderboard + '?page=2')
+
+    if (!lb || !lb2) return; // RIP
+    
+    const scores = lb.map((obj) => {
+        return obj.scoreParts[0].score;
+    });
+
+    const text = await fs.readFileSync('scores.json', 'utf-8');
+    const past = JSON.parse(text);
+    
+    const minutes = Math.round((Date.now() - start) / 60000)
+
+    past.push([minutes, scores]);
+        
+    await fs.writeFileSync('scores.json', JSON.stringify(past));
+    
+    // console.log("Data stored:", scores);
+}
+async function main() {
+    const races = await fetchURL(atob('aHR0cHM6Ly9kYXRhLm5pbmpha2l3aS5jb20vYnRkNi9yYWNlcw'))
+
+    if (!races) console.error(`[${Date.now()}] OH FUCK FAILED TO FETCH RACES`)
+
+    // Schedule the fetch every minute (60000 milliseconds)
+    const fetchLeaderboardIntervalID = setInterval(fetchLB, 60000, races[0]);
+}
+main() 
